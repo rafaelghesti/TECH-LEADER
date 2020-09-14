956b190fe1c2bd3d27c513c8b0f7ffcd5d1bf816
rafaelghesti <66754636+rafaelghesti@users.noreply.github.com>
2020-09-14 18:24:23 -0300

    first commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+import "./styles.css"
+
+const Header = () => (
+        <header id ="main-header">
+                <a href='/'>TECH LEADER</a>
+        </header>
+);
+
+export default Header;
